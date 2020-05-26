53f9ec6d6cad99a18fec028dcc218efa43ad48aa
pnshiralkar <pnshiralkar@gmail.com>
2020-05-26 20:51:42 +0530

    Added frontend hosted on s3 and cloudfront

---
+++ frontend/precache-manifest.d2b7654257bbff6a493b95f7479ccde2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c878f803099332f8701d62170696452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1a30fc197fefd59555c",
+    "url": "/static/css/2.980b5695.chunk.css"
+  },
+  {
+    "revision": "6bd45bb1b9f05f7cfd08",
+    "url": "/static/css/main.866477b9.chunk.css"
+  },
+  {
+    "revision": "e1a30fc197fefd59555c",
+    "url": "/static/js/2.d288c13a.chunk.js"
+  },
+  {
+    "revision": "c48a2591b3ab7add45dd0865a19affcf",
+    "url": "/static/js/2.d288c13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd45bb1b9f05f7cfd08",
+    "url": "/static/js/main.4407756f.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
